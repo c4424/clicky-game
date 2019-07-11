b92c718a1ad60976bc5a7dc70c90968d424643c6
c4424 <47879319+c4424@users.noreply.github.com>
2019-07-11 13:33:29 -0700

    Add files via upload
    Latest changes no 7/11/2019

--- src/logic/ClickyGame.js
+++ src/logic/ClickyGame.js
@@ -6,21 +6,28 @@ var   images = ['Eire Flag.jpg', 'Bob2011.jpg', 'Bob2017c.jpg', 'BobDad.jpg', 'C
 var   images2 = ['Eire Flag.jpg', 'Bob2011.jpg', 'Bob2017c.jpg', 'BobDad.jpg', 'Cal Flag 2.JPG', 'Cal Pride.jpg', 'Cliffs of Moor Castle.jpg',
         'Cliffs of Moor flags.jpg', 'Cliffs of Moor.jpg', 'Dollar Sign - Copy.jpg', 'Eire Flag.jpg', 'Occonnel St Dublin.JPG'];
 var   picked = [];
-
+var   newArr = new Array(12);
 export default class ClikyGame extends React.Component {
-
+	
     getImages(){return images2;}
     getImage(idx){return images2[idx];}
-
+	styles=['image-style1','image-style2','image-style3','image-style4','image-style5','image-style6','image-style7','image-style8','style9','style10','style11','style12'];
+	styles2=this.styles;
+	winCount=0;
+	getStyle (idx){
+		//alert("getStyle: "+newArr[idx]);
+		return this.styles2[idx];
+	}
     shuffle(arr=null)
     {
         if (arr === null)
             arr = images;
         var min = 0;
         var max = 11;
-        var newArr = new Array(arr.length);
+       
         //alert(newArr.length);hermione12
-
+		newArr = new Array(12);
+		this.styles2=new Array(12);
         for (var i in arr)
         {
             var random = parseInt((Math.random() * (+max - +min) + +min) + .5);
@@ -33,13 +40,14 @@ export default class ClikyGame extends React.Component {
                 alert("All Taken");
                 break;
             } else{
+//				alert("random: "+random);
                 newArr[i] = random;
-				images2[random]=images[i];
+				this.styles2[random]=this.styles[i];
 			}
 			//alert("shuffled");
             //alert(images2[0]);
         }
-
+//alert(this.styles2);
     }
 	isTaken(arr, pic)
     {
@@ -56,8 +64,8 @@ export default class ClikyGame extends React.Component {
             //alert("Game Over!");
             return true;
         }
+		++this.winCount;
 		picked.push(idx);
 		return false;
     }
-	
 }
